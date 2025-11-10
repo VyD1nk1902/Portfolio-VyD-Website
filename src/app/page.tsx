b50fe2dd6d67974e0c499f42d328a8cab96e065f
VyD1nk1902 <vy.dinh.2929@gmail.com>
2025-11-10 18:10:17 +0700

    Fix overflow content missing add new component section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,17 @@
 import HeroSection from "@/components/HeroSection";
+import ProjectSection from "@/components/ProjectSection";
+import TechStack from "@/components/TechStack";
 import WorkExperience from "@/components/WorkExperience";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="max-w-5xl mx-auto px-6">
+    <main className="max-w-5xl mx-auto px-6 max-[1000px]:overflow-x-hidden">
       <HeroSection />
       <WorkExperience />
+      <TechStack />
+      <ProjectSection />
 
-      <div id="lab" className="min-h-screen py-24">
-        Lab
-      </div>
-      <div id="project" className="min-h-screen py-24">
-        Project
-      </div>
       <div id="contact" className="min-h-screen py-24">
         Contact
       </div>
