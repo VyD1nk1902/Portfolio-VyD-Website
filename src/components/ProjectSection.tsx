72b10919cad6ba7a194540ea14feb318a95c5a71
VyD1nk1902 <vy.dinh.2929@gmail.com>
2025-11-11 15:23:06 +0700

    Project Section component

--- src/components/ProjectSection.tsx
+++ src/components/ProjectSection.tsx
@@ -1,8 +1,9 @@
 import Image from "next/image";
+import { MousePointerClick, MonitorPlay } from "lucide-react";
 
 const ProjectSection = () => {
   return (
-    <section id="project" className="relative py-28 space-y-24">
+    <section id="project" className="relative py-28 space-y-24 md:space-y-50">
       <div>
         <div
           className="pointer-events-none absolute right-0 left-1/3 top-8 -z-10 h-[650px] opacity-60 blur-3xl"
@@ -11,7 +12,7 @@ const ProjectSection = () => {
               "radial-gradient(60% 60% at 60% 40%, rgba(125,58,242,0.65) 0%, rgba(18,8,36,0)90%)",
           }}
         ></div>
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-16 items-center">
+        <div className="grid grid-cols-1 md:grid-cols-2 items-center">
           {/* Project Content */}
           <div className="relative z-10">
             {/* Heading */}
@@ -30,6 +31,21 @@ const ProjectSection = () => {
                 playlists of recommended tracks based on your existing playlists
                 and more.
               </p>
+              <div
+                className="pointer-events-none absolute -inset-px rounded-[14px] opacity-60"
+                style={{
+                  background:
+                    "linear-gradient(135deg, rgba(168,85,247,0.35), rgba(168,85,247,0.05))",
+                }}
+              ></div>
+            </div>
+            <div className="flex gap-4 p-4 text-white/60">
+              <span>
+                <MousePointerClick size={24} />
+              </span>
+              <span>
+                <MonitorPlay size={24} />
+              </span>
             </div>
           </div>
 
@@ -38,7 +54,7 @@ const ProjectSection = () => {
             tabIndex={0}
             className="group cursor-pointer rounded-2xl overflow-hidden bg-[#0f061c] border border-[#2f1c55] shadow-[50px_0_150px_rgba(168,85,247,0.28)] transition-transform duration-300 hover:-translate-y-1 focus:-translate-y-1"
           >
-            <div className="overflow-hidden h-[360px]">
+            <div className="overflow-hidden h-[250px] md:h-[360px]">
               <Image
                 src="/Portfolio.png"
                 alt="Portfolio"
@@ -46,6 +62,71 @@ const ProjectSection = () => {
                 height={640}
                 className="w-full h-auto transition-transform duration-2300 ease-linear group-hover:-translate-y-[65%] group-active:-translate-y-[65%] group-focus:-translate-y-[65%]"
               />
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="relative">
+        <div
+          className="pointer-events-none absolute -left-1/3 right-1/3 -top-10  -z-10 h-[650px] opacity-50 blur-3xl"
+          style={{
+            background:
+              "radial-gradient(60% 60% at 60% 40%, rgba(125,58,242,0.65) 0%, rgba(18,8,36,0)90%)",
+          }}
+        ></div>
+        <div className="grid grid-cols-1 md:grid-cols-2 items-center">
+          {/* Project Content */}
+          <div className="relative md:order-2 z-10">
+            {/* Heading */}
+            <div className="mb-12 text-right">
+              <span className="text-[#9857D3] text-base">Featured Project</span>
+              <p className="text-2xl md:text-[34px] font-semibold text-white/90">
+                Example Project
+              </p>
+            </div>
+            {/* Description */}
+            <div className="relative md:-ml-24 md:-mt-6 z-20 max-w-5xl rounded-2xl p-5 bg-white/6 backdrop-blur-sm border border-white/10 shadow-[0_6px_50px_rgba(126,34,206,0.25)]">
+              <p className="text-xs md:text-[18px] p-4 text-white/75">
+                A web app for visualizing personalized Spotify data. View your
+                top artists, top tracks, recently played tracks, and detailed
+                audio information about each track. Create and save new
+                playlists of recommended tracks based on your existing playlists
+                and more.
+              </p>
+              <div
+                className="pointer-events-none absolute -inset-px rounded-[14px] opacity-60"
+                style={{
+                  background:
+                    "linear-gradient(135deg, rgba(168,85,247,0.35), rgba(168,85,247,0.05))",
+                }}
+              ></div>
+            </div>
+            <div className="flex gap-4 p-4 text-white/60 justify-end">
+              <span>
+                <MousePointerClick size={24} />
+              </span>
+              <span>
+                <MonitorPlay size={24} />
+              </span>
+            </div>
+          </div>
+
+          {/* Project Image */}
+          <div className="relative md:order-1">
+            <div
+              tabIndex={0}
+              className="group cursor-pointer rounded-2xl overflow-hidden bg-[#0f061c] border border-[#2f1c55] shadow-[50px_0_150px_rgba(168,85,247,0.28)] transition-transform duration-300 hover:-translate-y-1 focus:-translate-y-1"
+            >
+              <div className="overflow-hidden h-[250px] md:h-[360px]">
+                <Image
+                  src="/Portfolio.png"
+                  alt="Portfolio"
+                  width={582}
+                  height={640}
+                  className="w-full h-auto transition-transform duration-2300 ease-linear group-hover:-translate-y-[65%] group-active:-translate-y-[65%] group-focus:-translate-y-[65%]"
+                />
+              </div>
             </div>
           </div>
         </div>
